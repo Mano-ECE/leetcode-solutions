8718f1e02049a06cce021a9c91e6297f4b50b19f
Manoj Kumar V <manojkumarvc2006@gmail.com>
2026-08-09 10:14:57 +0530

    Time: 2109 ms (21.31%), Space: 42.1 MB (45.44%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int res=0;
+        for(int i=1;i<num;i++){
+            if(num%i==0){
+                res= res + i;
+            }
+        }
+        if(res==num){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
